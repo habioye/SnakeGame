f472dac801dd13d082f93bfea7b6f4b9dfc205d8
stanleyshi42 <stanleyshi42@gmail.com>
2024-05-28 08:26:14 -0700

    initial commit

---
+++ src/main/java/MainMenuWindow.java
@@ -0,0 +1,30 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MainMenuWindow {
+
+	public static void main(String[] args) {
+		// Creating instance of JFrame
+		JFrame frame = new JFrame();
+
+		// Creating instance of JButton
+		JButton button = new JButton(" GFG WebSite Click");
+
+		// x axis, y axis, width, height
+		button.setBounds(150, 200, 220, 50);
+
+		// adding button in JFrame
+		frame.add(button);
+
+		// 400 width and 500 height
+		frame.setSize(500, 600);
+
+		// using no layout managers
+		frame.setLayout(null);
+
+		// making the frame visible
+		frame.setVisible(true);
+
+	}
+
+}
