06a6c9dcf0461cd633365f9e4cce783d542d061b
habioye <78454514+habioye@users.noreply.github.com>
2024-05-28 10:31:22 -0400

    initial template

---
+++ src/main/java/SnakeGame.java
@@ -0,0 +1,24 @@
+class SnakeGame.java {
+    // deque snake body
+    // ArrayList<ArrayList<int>> grid;
+
+
+    public void display(Arraylist grid) {
+
+    }
+    // generate fruit logic
+    public FruitData generatefruits();
+    // generate obstacles logic
+    public ObstacleData generateobstacles();
+    public PowerupData generatePowerup();
+    public inputData getInput();
+    public collisionData detectCollision();
+    public void generateDebuffs();
+    public float frametime;
+
+
+    public static void main (String[] args) {
+
+
+    }
+}
